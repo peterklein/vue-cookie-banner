be61914850a479d248c5fc7544b64d27d7fd3555
Peter Klein <hello@derpiet.de>
2020-02-17 18:31:57 +0100

    Removed build folder from gitignore

---
+++ build/rollup.config.js
@@ -0,0 +1,30 @@
+import vue from "rollup-plugin-vue";
+import buble from "rollup-plugin-buble";
+import uglify from "rollup-plugin-uglify-es";
+import minimist from "minimist";
+import commonjs from 'rollup-plugin-commonjs';
+
+const argv = minimist(process.argv.slice(2));
+
+const config = {
+	input: "src/index.js",
+	output: {
+		name: "VueCookieConsent",
+		exports: "named"
+	},
+	plugins: [
+		commonjs(),
+		vue({
+			css: true,
+			compileTemplate: true
+		}),
+		buble()
+	]
+};
+
+// minify browser (iife) version
+if (argv.format === "iife") {
+	config.plugins.push(uglify());
+}
+
+export default config;
